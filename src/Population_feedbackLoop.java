01da9236c5af9ad8ffc3901fd82237a32f0f1ba8
Amrinder Rai <amrinderrai94@gmail.com>
2014-03-09 19:26:49 +0000

    Okay this method to simulate population growth sucks

--- src/Population_feedbackLoop.java
+++ src/Population_feedbackLoop.java
@@ -2,4 +2,33 @@
  * Created by Amrinder on 09/03/2014.
  */
 public class Population_feedbackLoop {
+    private double populationTotal;
+    private double birthRate;
+    private double deathRate;
+
+    public Population_feedbackLoop(double birth_rate, double death_rate,double totalPopulation) {
+
+        birthRate = birth_rate;
+        deathRate = death_rate;
+        populationTotal = totalPopulation;
+    }
+
+    public int populationTicker() {
+
+        while(true) {
+
+          populationTotal += populationTotal*((birthRate - deathRate) /100 );
+            System.out.println(populationTotal);
+            continue;
+
+        }
+
+    }
+
+
+
+
+
+
+
 }
